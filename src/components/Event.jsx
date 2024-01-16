497ab51539975eb148d062a33a76a69daf72805b
TesMae <heyaw.inc@gmail.com>
2024-01-16 23:04:45 +0100

    display events

---
+++ src/components/Event.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+export default function Event(props) {
+  const [event, setEvent] = useState(props.event);
+
+  const handleLike = () => {
+    setEvent((eventPrev) => ({
+      ...eventPrev,
+      like: eventPrev.like ? 0 : 1,
+    }));
+  };
+
+  const handleBuy  = () => {
+    props.showBuyAlert();
+    setEvent((eventPrev) => ({
+        ...eventPrev,
+        nbParticipants: eventPrev.nbParticipants + 1,
+        nbTickets: eventPrev.nbTickets - 1,
+      }));
+  }
+
+  return (
+    <Col style={{ width: "20%" }} className="m-2">
+      <Card>
+        <Card.Img
+          variant="top"
+          style={{ height: 400 }}
+          src={`/images/${!!event.nbTickets ? event.img : 'sold_out.png'}`}
+        />
+        <Card.Body>
+          <Card.Title>{event.name}</Card.Title>
+          <Card.Text>Price : {event.price}</Card.Text>
+          <Card.Text>Number of tickets : {event.nbTickets}</Card.Text>
+          <Card.Text>Number of participants : {event.nbParticipants}</Card.Text>
+          <Card.Text>Like : {event.like}</Card.Text>
+          <Button variant="info" onClick={handleLike} className="mx-5">
+            Like
+          </Button>
+          <Button
+            variant="primary"
+            onClick={handleBuy}
+            disabled={!!event.nbTickets ? false : true}
+            className="mx-5"
+          >
+            Buy
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
---
+++ src/components/Events.jsx
@@ -0,0 +1,47 @@
+import eventsJson from "../data/events.json";
+import React, { useEffect, useState } from "react";
+import Event from "./Event";
+import Row from "react-bootstrap/Row";
+import Alert from "react-bootstrap/Alert";
+
+export default function Events() {
+  const [isWelcome, setIsWelcome] = useState(true);
+  const [isShowBuyAlert, setIsShowBuyAlert] = useState(false);
+
+  useEffect(() => {
+    const isWelcomeTimeout = setTimeout(() => {
+      setIsWelcome(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(isWelcomeTimeout);
+    };
+  }, []);
+
+  const showBuyAlert = () => {
+    setIsShowBuyAlert(true);
+    setTimeout(() => {
+      setIsShowBuyAlert(false);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      {isWelcome && (
+        <Alert style={{ width: "70%", marginBottom: 40 }} variant="success">
+          Hey welcome to the Shop
+        </Alert>
+      )}
+      <Row xs={1} md={4} className="g-4">
+        {eventsJson.map((eventItem, index) => (
+          <Event key={index} event={eventItem} showBuyAlert={showBuyAlert} />
+        ))}
+      </Row>
+      {isShowBuyAlert && (
+        <Alert style={{ width: "70%", marginTop: 20 }} variant="primary">
+          You bought an Item
+        </Alert>
+      )}
+    </div>
+  );
+}
